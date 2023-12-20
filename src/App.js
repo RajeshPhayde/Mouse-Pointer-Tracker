5da5fb9add1ff8cb8dd82ea2f301278b050d4b9e
RajeshPhayde <rajeshphayde97@gmail.com>
2023-12-20 08:50:21 +0530

    title tag updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const WithMousePosition = (WrappedComponent)=>{
+
+    const [mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0
+    })
+
+    useEffect(()=>{
+
+      const handleMousePositionChange = (e)=>{
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY
+        })
+      }
+
+      window.addEventListener("mousemove", handleMousePositionChange)
+      
+      return ()=>{
+        window.removeEventListener("mousemove", handleMousePositionChange)
+      }
+    },[])
+
+    return (props)=>{
+
+      return <WrappedComponent {...props} mousePosition={mousePosition}/>
+    }
+
+  }
+
+  const PanelMouseLogger =({mousePosition})=>{
+    if(!mousePosition){
+      return null;
+    }
+    return (
+      <div className="Tracker">
+        <p>Mouse Position:</p>
+          <div className="Row">
+            <span>x: {mousePosition.x}</span>
+            <span>y: {mousePosition.y}</span>
+          </div>
+      </div>
+    );
+  }
+
+  const PointMouseLogger = ({mousePosition})=>{
+    if(!mousePosition){
+      return null;
+    }
+    return (
+      <p>
+        ({mousePosition.x}, {mousePosition.y})
+      </p>
+    );
+  };
+
+  const PanelMouseTracker = WithMousePosition(PanelMouseLogger)
+  const PointMouseTracker = WithMousePosition(PointMouseLogger)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>Mouse Point Tracker</header>
+        <PanelMouseTracker/>
+        <PointMouseTracker/>
     </div>
   );
 }
